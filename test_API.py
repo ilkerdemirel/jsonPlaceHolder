205280ff28e44d27451beeb16e0c313ce6f80f29
kuncun <kuncun@kuncunella.fritz.box>
2019-05-23 21:47:08 +0200

    First test is created.

--- test_API.py
+++ test_API.py
@@ -1,3 +1,19 @@
 import nose
 import requests
-print('ilker')
+
+
+
+BASE_URL = 'https://jsonplaceholder.typicode.com'
+ROUTE_ALBUMS = "/albums"
+
+STATUS_CODE_200 = 200
+
+
+# ALBUMS TESTS
+response_albums = requests.get(BASE_URL + ROUTE_ALBUMS)
+
+def test_albums_status_code_funtional():
+    """
+    Checks that Albums route status is correct as expected.
+    """
+    assert response_albums.status_code == STATUS_CODE_200
